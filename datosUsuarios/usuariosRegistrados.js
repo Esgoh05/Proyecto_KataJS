737e6e25cf6cc14c4ca8438e1cd7154941c9c549
Estefanía Gordillo <ada_hack143@hackademy.mx>
2023-06-24 22:25:11 -0600

    usuariosRegistrados

---
+++ datosUsuarios/usuariosRegistrados.js
@@ -0,0 +1,25 @@
+const arregloUsuariosRegistrados = [
+    {
+        nombre: "Fanny Gordillo",
+        telefono: 9622796913,
+        email: "esgoh.gordillo@gmail.com",
+        role: "Administrador"
+    },
+    {
+        nombre: "Maria Fernandez",
+        telefono: 9622295567,
+        email: "a@prueba.com",
+        role: "User"
+    },
+    {
+        nombre: "Linda Hernandez",
+        telefono: 9623488571,
+        email: "a1@prueba.com",
+        role: "User"
+    },
+    
+   
+   
+];
+
+export default arregloUsuariosRegistrados;
