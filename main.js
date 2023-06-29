f679dd6f985f002e0bd346e0e30fb518a4d7d521
Estefanía Gordillo <ada_hack143@hackademy.mx>
2023-06-28 20:50:55 -0600

    update user

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import arregloUsuariosRegistrados from './datosUsuarios/usuariosRegistrados.js';
 
 const registroUsuariosForm = document.querySelector('#registroUsuariosForm');
-const cerrarModal = document.querySelector('#exampleModal');
+const editarUsuariosForm = document.querySelector('#editarUsuariosForm');
+
 
 
 const enviarFormRegistroUsuarios = (event) => {
@@ -9,9 +10,9 @@ const enviarFormRegistroUsuarios = (event) => {
 
     /* Almacenando la información del formulario en un objeto de JavaScript */
     const usuario = {
-        nombre: registroUsuariosForm.name.value,
-        telefono: Number(registroUsuariosForm.phone.value),
-        email: registroUsuariosForm.email.value
+        nombre: registroUsuariosForm.nameUser.value,
+        telefono: Number(registroUsuariosForm.phoneUser.value),
+        email: registroUsuariosForm.emailUser.value
     };
 
     /*TODO: Valide tambien que el nombre del jedi no este repetido */
@@ -26,6 +27,9 @@ const enviarFormRegistroUsuarios = (event) => {
           });
         arregloUsuariosRegistrados.push(rolePredeterminado(usuario, 1));
         actualizaTablaUsuarios();
+        console.log("actualizado");
+        saveToLocalStorage(registroUsuariosForm);
+        console.log("guardado");
         console.log(arregloUsuariosRegistrados)
         console.log("aaaa");
     } else {
@@ -62,6 +66,40 @@ const rolePredeterminado = (roleInicio, role) => {
     }
 };
 
+const editarUsuario = (event) => {
+    const index = event.target.dataset.index;
+    const usuario = arregloUsuariosRegistrados[index];
+
+    const newName = (usuario.nombre);
+    console.log(newName)
+    const newPhone = (usuario.telefono);
+    const newRole = (usuario.role);
+    console.log(newRole)
+    const newEmail = (usuario.email);
+
+    document.getElementById("nameEdit").value = newName;
+    document.getElementById("phoneEdit").value = newPhone;
+    document.getElementById("emailEdit").value = newEmail;
+   
+
+    /*if (newName && newPhone && newEmail && newRole) {
+        // Si se proporcionan nuevos valores válidos, se actualiza el jedi en el arreglo de jedis
+        arregloUsuariosRegistrados[index] = {
+            ...jedi,
+            nombre: newName,
+            telefono: Number(newPhone),
+            email: newEmail,
+            role: newRole
+        };
+
+        actualizaTablaUsuarios();
+        console.log(arregloUsuariosRegistrados);
+    }*/
+};
+
+/* Validamos que no haya propiedades vacías */
+const campoNoRegistradoEdit = ({ nombre = "", telefono = 0, email = "", role = "" }) => nombre !== "" && telefono !== 0 && email !== "" && role !== ""; // Regresa un booleano
+
 const deleteUsuario = (event) => {
     Swal.fire({
         title: '¿Usted está seguro de querer eliminar este usuario?',
@@ -86,11 +124,6 @@ const deleteUsuario = (event) => {
             )
         }
       })
-    /*const index = event.target.dataset.index;
-    arregloUsuariosRegistrados.splice(index, 1);
-
-    actualizaTablaUsuarios();
-    console.log(arregloUsuariosRegistrados);*/
 };
 
 const actualizaTablaUsuarios = (filteredJedis = arregloUsuariosRegistrados) => {
@@ -126,25 +159,73 @@ const actualizaTablaUsuarios = (filteredJedis = arregloUsuariosRegistrados) => {
     const editButtons = document.querySelectorAll('.editBtn');
     const deleteButtons = document.querySelectorAll('.deleteBtn');
 
-    /*editButtons.forEach((button) => {
-        button.addEventListener('click', handleEditJedi);
-    });*/
+    editButtons.forEach((button) => {
+        button.addEventListener('click', editarUsuario);
+    });
 
     deleteButtons.forEach((button) => {
         button.addEventListener('click', deleteUsuario);
     });
 };
 
+function saveToLocalStorage({ nameUser, phoneUser, emailUser }) {
+    const { value: name } = nameUser;
+    const { value: phone } = phoneUser;
+    const { value: email } = emailUser;
+
+    const user = arregloUsuariosRegistrados.find(user => user.nombre == name && user.telefono == phone && user.email == email);
+    console.log("pase")
+
+    // Guarda los valores en el almacenamiento local
+    for (const prop in user) {
+        console.log("dentro")
+        localStorage.setItem(prop, user[prop]);
+    }
+}
+
+const enviarFormEditarUsuarios = (event) => {
+    event.preventDefault(); // Evita que se envíe el formulario automáticamente
+    console.log("ya entre")
+    const usuario = {
+        nombre: editarUsuariosForm.nameEdit.value,
+        telefono: Number(editarUsuariosForm.phoneEdit.value),
+        email: editarUsuariosForm.emailEdit.value,
+        role: editarUsuariosForm.usertypeEdit.value
+    };
+
+    /*TODO: Valide tambien que el nombre del jedi no este repetido */
+    if (campoNoRegistradoEdit(usuario)) {
+        // Si los datos del personaje son válidos, se agrega al arreglo de jedis y se actualiza la lista
+       Swal.fire({
+            icon: 'success',
+            title: 'Usuario registrado con exito',
+            showConfirmButton: true,
+            confirmButtonColor: '#0d6efd',
+            confirmButtonText: 'Ok',
+          });
+        arregloUsuariosRegistrados.push(usuario);
+        actualizaTablaUsuarios();
+        console.log(arregloUsuariosRegistrados)
+        console.log("editando");
+    } else {
+        // Si hay datos vacíos, se muestra una alerta y se detiene la propagación del evento
+        alert("Tienes un dato vacío, verifícalo");
+        event.stopPropagation();
+    }
+};
+
+const userNameElement = document.querySelector('#nameEdit');
+
+const userName = localStorage.getItem('nombre');
+if (userName) {
+    userNameElement.textContent = `${userName}`;
+}
+
 /* Manejador de evento */
 registroUsuariosForm.addEventListener('submit', enviarFormRegistroUsuarios);
 
-const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger'
-    },
-    buttonsStyling: false
-  })
+editarUsuariosForm.addEventListener('submit', enviarFormEditarUsuarios);
+
 
 
 
